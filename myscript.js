a600f1ee9c377184f0023193ced5ce6e7821d897
jtjemzz09 <jtjemzz09@LAPTOP-292I2KQV>
2023-03-22 01:26:05 -0700

    first commit

---
+++ myscript.js
@@ -0,0 +1,14 @@
+//make a new file : index.html and myscript.js
+//create a string
+//assign three variables contains the corresponding result of calculation using arithmetic operator.
+//create a dialog box displaying vault codes
+//submit a screenshot or github link
+
+const str="You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+//I created a string message
+//assign three variables
+const numA=15-5;
+const numB=20*2;
+const numC=30+9;
+//created dialog box 
+alert(str+numA+"-"+numB+"-"+numC);
